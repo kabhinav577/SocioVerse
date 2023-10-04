a7d211a08e4728f573dc2bc487f9f13ae0a4b21e
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-10-04 11:04:30 +0530

    Completing jwt authorization and setup public folder

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,9 @@ import multer from 'multer';
 import helmet from 'helmet';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
 import morgan from 'morgan';
+import { register } from './controllers/auth.js';
 
 /*    CONFIGURATION     */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +36,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+//*  ROUTES WITH FILES *//
+app.use('/auth', upload.single('picture'), register);
+
+//* ROUTES *//
+app.use('/auth', authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
