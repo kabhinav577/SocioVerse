79d84fe3f8fec9f643e09936283634cff0f5081a
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-10-05 19:37:46 +0530

    Dummy Data added to mongoDB for client creating

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 /*    CONFIGURATION     */
 const __filename = fileURLToPath(import.meta.url);
@@ -59,5 +62,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server is Running on Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((err) => console.log(`Server did't Connect ${err}`));
