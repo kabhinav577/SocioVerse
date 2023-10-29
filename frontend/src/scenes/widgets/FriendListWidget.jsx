1c98eaf4bce7d4c0312611034a89f549eab1b088
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-10-29 21:01:45 +0530

    solving issue undefined like userid

--- frontend/src/scenes/widgets/FriendListWidget.jsx
+++ frontend/src/scenes/widgets/FriendListWidget.jsx
@@ -41,9 +41,9 @@ const FriendListWidget = ({ userId }) => {
         Friend List
       </Typography>
       <Box display="flex" flexDirection="column" gap="1.5rem">
-        {friends.map((friend) => (
+        {friends.map((friend, i) => (
           <Friend
-            key={friend._id}
+            key={i}
             friendId={friend._id}
             name={`${friend.firstName} ${friend.lastName}`}
             subtitle={friend.occupation}
